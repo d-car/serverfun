b14fd5ed8e36cc53b82ce89733c51e49092ba76f
d-car <danielscarstensen@gmail.com>
2018-08-14 19:56:33 -0500

    localhost

--- twoservers.js
+++ twoservers.js
@@ -9,17 +9,17 @@ var server1 = http.createServer(makeRequest1);
 var server2 = http.createServer(makeRequest2)
 
 function makeRequest1(req, res) {
-    res.end("blah" + req.url)
+    res.end("Port: " + PORT1 + " says that you are cool..." + req.url)
 };
 
 function makeRequest2(req, res) {
-    res.end("blah" + req.url)
+    res.end("Port: " + PORT2 + " says but not that cool... " + req.url)
 };
 
 server1.listen(PORT1, function() {
-    console.log("Port: " + PORT1 + " says that you are cool...")
+    console.log(PORT1)
 })
 
 server2.listen(PORT2, function() {
-    console.log("Port: " + PORT2 + " says but not that cool... ")
+    console.log(PORT2)
 })
